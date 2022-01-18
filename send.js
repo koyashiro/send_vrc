4d05e4ba938094c868d56d07e6f75e4d87cf102d
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2022-01-18 22:49:37 +0900

    add version wip

--- send.js
+++ send.js
@@ -1,14 +1,30 @@
 (() => {
-    let toVRC = (url) => {
-        fetch("http://localhost:11400/url", {
-            method: "POST",
-            mode: "cors",
+    let finishdUpdateNotify = false;
+    const defaultMethod = 'POST';
+    const expectedVersion = 'v0.1.1';
+
+    let toVRC = (url, method = defaultMethod) => {
+        fetch('http://localhost:11400/url', {
+            method: method,
+            mode: 'cors',
             credentials: 'omit',
-            cache: "no-cache",
+            cache: 'no-cache',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({url: url})
+        }).then((res) => {
+            console.log(res.headers)
+            alert(res.headers.get("X-VERSION"))
+            if (res.headers.get('X-VERSION') !== expectedVersion) {
+                if (!finishdUpdateNotify && confirm('please update send_vrc_desktop')) {
+                    finishdUpdateNotify = true;
+                    window.open('https://bootjp.booth.pm/items/3572196')
+                }
+            }
         }).catch((e) => {
-            console.log(e);
+            console.warn(e);
+            if (method == defaultMethod) {
+                toVRC(url, 'PUT');
+            }
         });
     }
 
